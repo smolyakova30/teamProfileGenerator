a5f5243f376d71ea56ea4506d06d44f67477f696
Olga <o.smolyakova30@gmail.com>
2021-08-01 12:35:17 -0500

    added css file and created generateHTML file, with functions and template for HTML page for different roles

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
+const generateHTML = require('.src/generateHTML')
 
 
 const questions = [
